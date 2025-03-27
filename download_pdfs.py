1c09267d6d2fd4e571cb956fa834aed2f794cd0d
Elder Galvão <eldergalvao07@gmail.com>
2025-03-27 07:37:02 -0300

    Iniciando download de PDFs e requisição.

--- download_pdfs.py
+++ download_pdfs.py
@@ -10,9 +10,8 @@ def baixar_pdfs(response):
         #analisando conteúdo da página / beautifulsoup
         soup = BeautifulSoup(response.text, "html.parser")
 
-        #encontrando links PDFS na página
+        #ENCONTRAR LINKS PDFS PRESENTES NA PÁGINA-----------------------------------
         pdf_links = []
-
         #looping para fazer a verificação da existência de links terminados em .pdf e adiciona-lós a lista
         #encontra todos os links da página
         for link in soup.find_all("a"):
@@ -20,21 +19,27 @@ def baixar_pdfs(response):
             if link.get("href").endswith(".pdf"):
                 #adiciona o link a lista
                 pdf_links.append(link.get("href"))
-
-        #CONVERSÃO DE LINKS RELATIVOS EM ABSOLUTOS-----------------------------------
         if not pdf_links:
             print("Nenhum link PDF encontrado.")
         else:
             #retorna a quantidade de pdfs encontrados
             print(f"Encontrados {len(pdf_links)} links PDF.")
 
-            #converter pdf relativos em absolutos para trabalhar com http
+            #CONVERSÃO DE LINKS RELATIVOS EM ABSOLUTOS PARA TRABALHAR COM HTTP------------------------
             for pdf_absoluto in pdf_links:
                 if not pdf_links.startswith("http"):
                     pdf_absoluto = requests.compat.urljoin(response, pdf_absoluto)
 
             #extraindo nome dos pdfs do links em pdf_absoluto
             nome_arquivo = os.path.join(pdf_absoluto.split("/")[-1])#talvez ocorra erro aqui###############
+
+            #BAIXAR PDF---------------------------------------------------------------
+            print(f"Baixando PDF: {nome_arquivo}") 
+            #fazendo requisição no link do pdf convertido para absoluto
+            pdf_final = requests.get(pdf_absoluto)
+            #verificar erro na requisição
+            pdf_final.raise_for_status()
+
 
     except Exception as e:
         print(f"Erro ao acessar o site ou baixar os PDFs: {e}")
