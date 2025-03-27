bc0fb0bec2dc1382fb69f7e612cfe81f462bbdd0
3lder7 <eldergalvao07@gmail.com>
2025-03-27 09:30:29 -0300

    ✅Todos os pdfs sendo salvos em pasta, e correção do bug previsto anteriormente.

--- download_pdfs.py
+++ download_pdfs.py
@@ -10,7 +10,7 @@ def baixar_pdfs(response):
         #analisando conteúdo da página / beautifulsoup
         soup = BeautifulSoup(response.text, "html.parser")
 
-        #ENCONTRAR LINKS PDFS PRESENTES NA PÁGINA-----------------------------------
+        #-------------------------------ENCONTRAR LINKS PDFS PRESENTES NA PÁGINA-----------------------------------------------
         pdf_links = []
         #looping para fazer a verificação da existência de links terminados em .pdf e adiciona-lós a lista
         #encontra todos os links da página
@@ -25,21 +25,26 @@ def baixar_pdfs(response):
             #retorna a quantidade de pdfs encontrados
             print(f"Encontrados {len(pdf_links)} links PDF.")
 
-            #CONVERSÃO DE LINKS RELATIVOS EM ABSOLUTOS PARA TRABALHAR COM HTTP------------------------
+            #--------------------------CONVERSÃO DE LINKS RELATIVOS EM ABSOLUTOS PARA TRABALHAR COM HTTP------------------------
             for pdf_absoluto in pdf_links:
-                if not pdf_links.startswith("http"):
+                if not pdf_absoluto.startswith("http"):
                     pdf_absoluto = requests.compat.urljoin(response, pdf_absoluto)
 
-            #extraindo nome dos pdfs do links em pdf_absoluto
-            nome_arquivo = os.path.join(pdf_absoluto.split("/")[-1])#talvez ocorra erro aqui###############
+                #extraindo nome dos pdfs do links em pdf_absoluto e salvando na variável
+                nome_arquivo = os.path.join("pdfs", pdf_absoluto.split("/")[-1])
 
-            #BAIXAR PDF---------------------------------------------------------------
-            print(f"Baixando PDF: {nome_arquivo}") 
-            #fazendo requisição no link do pdf convertido para absoluto
-            pdf_final = requests.get(pdf_absoluto)
-            #verificar erro na requisição
-            pdf_final.raise_for_status()
+                #---------------------------------------------BAIXAR PDF---------------------------------------------------------
+                print(f"Baixando PDF: {nome_arquivo}") 
+                #fazendo requisição no link do pdf convertido para absoluto
+                pdf_final = requests.get(pdf_absoluto)
+                #verificar erro na requisição
+                pdf_final.raise_for_status()
 
+                #---------------------------------------SALVANDO PDF NO DISCO-----------------------------------------------------
+                #wb = write/binary para abrir o arquivo em modo binário (essencial para arquivos PDF)
+                with open(nome_arquivo, "wb") as arquivo_pdf: 
+                    arquivo_pdf.write(pdf_final.content)#escreve o conteúdo do pdf baixado no arquivo
+                print(f"PDF salvo como: {nome_arquivo}")  
 
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print(f"Erro ao acessar o site ou baixar os PDFs: {e}")
