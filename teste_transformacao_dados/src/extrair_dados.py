a22464381043f8b19694e2420f457735585db061
3lder7 <eldergalvao07@gmail.com>
2025-03-28 09:52:58 -0300

    • Removendo todos os arquivos de dentro de uma única pasta, para evitar futuros conflitos • Primeiro código teste de extração de dados da tabela.

---
+++ teste_transformacao_dados/src/extrair_dados.py
@@ -0,0 +1,31 @@
+import pdfplumber #manipulação de pdf
+import pandas as pd #manipulação de dados
+
+def extrair_tabela (caminho_pdf):
+    linhas_da_tabela = [] #lista que armazena as linhas da tabela
+
+    with pdfplumber.open(caminho_pdf) as pdf: #abre o pdf
+        for pagina in pdf.pages:
+            tabelas = pagina.extract_tables() #extrai as tabelas da página com função do pdfplumber
+
+            #-------------esse bloco foi gerado 100% com IA, não entendi como funciona muito bem----------------
+            for tabela in tabelas:
+                for linha in tabela:
+                    #pula linhas vazias ou cabeçalhos repetidos
+                    if linha[0] and "PROCEDIMENTO" not in linha[0]:
+                        linhas_da_tabela.append(linha)
+            #---------------------------------------------------------------------------------------------------
+    
+    #colunas da tabela, são as mesmas do pdf
+    #mas sem os espaços em branco
+    colunas =[
+        "PROCEDIMENTO", "RN", "VIGÊNCIA", "OD", "AMB", "HCO", 
+        "HSO", "REF", "PAC", "DUT", "SUBGRUPO", "GRUPO", "CAPÍTULO"
+    ]
+
+    #criação do dataframe para organizar as informações em linhas e colunas
+    #e preparar os dados para serem transformados em um arquivo csv
+    df = pd.DataFrame(linhas_da_tabela, columns=colunas)
+    #remove linhas completamente vazias
+    df = df.dropna(how='all')
+    return df
