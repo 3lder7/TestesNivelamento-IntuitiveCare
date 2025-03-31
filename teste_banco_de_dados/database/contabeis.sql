e3ae37ae5d25b3cd4d8dfb6a66a95325faa7f4f4
Elder Galvão <eldergalvao07@gmail.com>
2025-03-31 17:16:19 -0300

    ✅Corrigindo arquivos zipados para csv e começando a resolver erro de importação do CSV para a tabela

--- teste_banco_de_dados/database/contabeis.sql
+++ teste_banco_de_dados/database/contabeis.sql
@@ -6,14 +6,18 @@ CREATE TABLE demonstracoes_1T2023(
     DESCRICAO TEXT,             
     VL_SALDO_INICIAL DECIMAL(15, 2),  
     VL_SALDO_FINAL DECIMAL(15, 2),    
-    FOREIGN KEY (REG_ANS) REFERENCES operadoras_ativas(Registro_ANS)  --chave estrangeira para a tabela de operadoras ativas
+    FOREIGN KEY (REG_ANS) REFERENCES operadoras_ativas(Registro_ANS)  -- chave estrangeira para a tabela de operadoras ativas
 );
 
 /*----------------IMPORTANDO CSV----------------*/
-LOAD DATA INFILE '../2023/1T2023.csv' -- inserir dados do csv nas colunas da tabela
+LOAD DATA LOCAL INFILE '../2023/1T2023/1T2023.csv' -- inserir dados do csv nas colunas
 INTO TABLE demonstracoes_1T2023
-FIELDS TERMINATED BY ';'    -- delimitador 
-ENCLOSED BY '"'           
-LINES TERMINATED BY '\n'    -- fazr a quebra de linha
-IGNORE 1 ROWS               -- ignorar o cabeçalho
-(DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL); --especificando a ordem das colunas
+FIELDS TERMINATED BY ';' -- delimitador
+ENCLOSED BY '"' -- ler os valores entre aspas
+LINES TERMINATED BY '\n' -- pula linha
+IGNORE 1 LINES -- pra ignorar o cabeçalho
+(@DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL)
+
+SET GLOBAL local_infile = 1; -- habilitar o uso do LOAD DATA LOCAL INFILE
+SHOW VARIABLES LIKE 'local_infile'; -- verificar
+
