08775ad97c622544d16641471137574fd6a27624
3lder7 <eldergalvao07@gmail.com>
2025-04-01 12:00:18 -0300

    Alterando a conversão de data, que estava sendo desnecessária, e mudando para LOAD DATA LOCAL INFILE, com o objetivo de puxar o arquivo dentro do projeto

--- teste_banco_de_dados/database/contabeis.sql
+++ teste_banco_de_dados/database/contabeis.sql
@@ -1,6 +1,4 @@
 USE banco_ans;  --selecionando o banco 
-SET GLOBAL local_infile = 1; -- habilitar o uso do LOAD DATA LOCAL INFILE para armazenar os dados no banco
-SHOW VARIABLES LIKE 'local_infile'; -- verificar
 CREATE TABLE demonstracoes_1T2023(
     DATA DATE,                  
     REG_ANS VARCHAR(10),        
@@ -12,12 +10,13 @@ CREATE TABLE demonstracoes_1T2023(
 );
 
 /*----------------IMPORTANDO CSV----------------*/
-LOAD DATA LOCAL INFILE 'C:/Users/elder/Downloads/TestesNivelamento-IntuitiveCare/teste_banco_de_dados/2023/1T2023.csv' -- inserir dados do csv nas colunas
+LOAD DATA LOCAL INFILE '2023/1T2023.csv' -- inserir dados do csv nas colunas
 INTO TABLE demonstracoes_1T2023
 FIELDS TERMINATED BY ';' -- delimitador
 ENCLOSED BY '"' -- ler os valores entre aspas
 LINES TERMINATED BY '\n' -- pula linha
 IGNORE 1 LINES -- pra ignorar o cabeçalho
+(DATA, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL) -- mapamento direto das colunas
 
 SELECT*FROM demonstracoes_1T2023 LIMIT 10;
 
